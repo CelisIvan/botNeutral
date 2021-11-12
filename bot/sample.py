7e909798c47537c515f4559a05e49314cab173b6
Ivan Celis <ivan.celis.mal@gmail.com>
2021-11-12 03:10:30 -0600

    neutral

--- bot/sample.py
+++ bot/sample.py
@@ -9,7 +9,7 @@ spanish_bot = ChatBot("Chatterbot",storage_adapter="chatterbot.storage.SQLStorag
                     {
                         'import_path': 'chatterbot.logic.BestMatch',
                          'default_response': default,
-                         'maximum_similarity_threshold': 0.90
+                         'maximum_similarity_threshold': 0.70
                     },
                 ])
 trainer = ChatterBotCorpusTrainer(spanish_bot)
@@ -25,14 +25,20 @@ def index():
 def get_bot_response():
      userText = request.args.get("msg") #get data from input,we write js  to index.html
      if userText == "SECCIÓN 2":
-          return "http://localhost:4200/atls2"
+          return  "https://tesina-331906.oa.r.appspot.com/atls2"
      elif userText == "SECCIÓN 3":
-          return "http://localhost:4200/apasels3"
+          return "https://tesina-331906.oa.r.appspot.com/apasels3"
      elif userText == "SECCIÓN 4":
-          return "http://localhost:4200/fpvls4"
+          return "https://tesina-331906.oa.r.appspot.com/fpvls4"
      elif userText == "SECCIÓN 1":
-          return "http://localhost:4200/eels1"     
-     return str(spanish_bot.get_response(userText))
+          return "https://tesina-331906.oa.r.appspot.com/eels1"
+     elif userText == "EVALUACIÓN":
+          return "https://forms.gle/TpT4UvoZBvKQ6kLU8"   
+     responseText = str(spanish_bot.get_response(userText))
+     if responseText == "What is AI?":
+          return default
+     else:
+          return responseText
 
 
 if __name__ == "__main__":
